d60a02835580d0486b3d9f448a499f04b4687cf6
caritosteph <carito.ulfe@gmail.com>
2016-05-01 23:37:21 -0500

    project and resume

--- _site/js/main.js
+++ _site/js/main.js
@@ -21,11 +21,22 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
+    $('a.blog-resume').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
 
-  if (window.location.hash && window.location.hash == '#blog') {
+  if (window.location.pathname && window.location.pathname == '/blog/') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  if (window.location.hash && window.location.hash == '#projects') {
+  if (window.location.pathname && window.location.pathname == '/projects/') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
   if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
@@ -45,5 +56,6 @@ $(document).ready(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
+ 
 
 })
